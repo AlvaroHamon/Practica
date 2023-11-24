3bc5319a4ebb51aff1357993a6ba1151916f24d4
AlvaroHamon <alvaro.hamon@gmail.com>
2023-11-23 19:01:32 -0500

    construyendo proyecto para desplegar

--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -1,28 +1,19 @@
+import Boton from "../../elements/Boton/Boton";
 import "./index.css";
 
 const NavBar = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>Inicio</li>
-          <li>Iniciar sesion</li>
-        </ul>
-      </nav>
+    <header className="contenedorheader">
+      <Boton texto="Inicio" />
+      <Boton texto="Iniciar Sesion" />
+      {/* <ul>
+        <li>Inicio</li>
+        <li>Iniciar sesion</li>
+      </ul> */}
+      {/* <nav>
+      </nav> */}
     </header>
   );
 };
 
 export default NavBar;
-
-// export default function NabVar() {
-//   return (
-//     <>
-//       <nav>
-//         <ul>
-//           <li>Inicio</li>
-//           <li>Iniciar sesion</li>
-//         </ul>
-//       </nav>
-//     </>
-//   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import NavBar from "./assets/components/NavBar/NavBar";
 import Footer from "./assets/components/Footer/Footer";
-import Login from "./assets/components/Login/Login";
+// import Login from "./assets/components/Login/Login";
 import Registro from "./assets/components/Registro/Registro";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
